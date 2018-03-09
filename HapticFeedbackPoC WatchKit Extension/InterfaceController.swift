be3e57b88659e2b7e441fa0dbb2db45882511af7
Amin Siddiqui <amin.s@codewalla.com>
2018-03-09 16:20:00 +0530

    - Initial Commit

---
+++ HapticFeedbackPoC WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController.swift
+//  HapticFeedbackPoC WatchKit Extension
+//
+//  Created by Amin Siddiqui on 09/03/18.
+//  Copyright © 2018 svmLogics. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
